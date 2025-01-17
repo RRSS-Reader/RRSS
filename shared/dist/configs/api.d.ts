6db92ff9e94cd84a8052bb4999013a7f39a6ed4f
Oyasuminasai <61616918+nfnfgo@users.noreply.github.com>
2025-01-17 18:40:41 +0800

    fix: Remove build files.

--- shared/dist/configs/api.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const port: number;
-export declare const swaggerPath: string;
-export declare const version: string;
--- shared/dist/configs/api.js
+++
@@ -1,4 +0,0 @@
-export const port = 3000;
-export const swaggerPath = "docs";
-export const version = "1.0.0";
-//# sourceMappingURL=api.js.map
